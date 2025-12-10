72cdd677ec36b3b957312dc951828a7ed8b28397
Aniket1308-dev <aniketdevadas@gmail.com>
2025-12-11 00:32:20 +0530

    add email sanitization

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -184,4 +184,24 @@ describe('POST /auth/register', () => {
             expect(users).toHaveLength(0)
         })
     })
+    describe('Fields are not in proper format', () => {
+        it('should trim the email field', async () => {
+            //Arrange
+            const userData = {
+                firstName: 'Aniket',
+                lastName: 'Vakkithody',
+                email: ' aniketdevadas@gmail.com ',
+                password: 'secret',
+            }
+
+            //Act
+            await request(app).post('/auth/register').send(userData)
+
+            //Assert
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find()
+            const user = users[0]
+            expect(user.email).toBe('aniketdevadas@gmail.com')
+        })
+    })
 })
